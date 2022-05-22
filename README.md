d050a241c93c152bb44616bdb3757fea1881a82d
Renan Hartwig <renan.hartwig@dootax.com.br>
2022-05-22 12:38:15 -0300

    Module 2 finished

---
+++ README.md
@@ -0,0 +1,3 @@
+# catalog-admin
+
+An application to manage a video catalog built in FullCycle 3.0 course [https://imersao.fullcycle.com.br/page/lancamento/] 
---
+++ infrastructure/src/main/java/com/hartwig/catalog/admin/infrastructure/configuration/WebServerConfig.java
@@ -0,0 +1,9 @@
+package com.hartwig.catalog.admin.infrastructure.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.hartwig.catalog.admin")
+public class WebServerConfig {
+}
